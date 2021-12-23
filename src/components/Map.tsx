ea3bd51a9d2a2ecaa45035a957f26036a2947981
Yana Shakhovskaya <yaishakhovskaya@avito.ru>
2021-12-23 16:13:25 +0300

    add getting info for the next launches

--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -1,14 +1,16 @@
-import React, { useEffect, useState } from "react";
+import React, {useEffect, useState} from "react";
 import {
     ComposableMap,
     Geographies,
     Geography,
     Marker,
+    ZoomableGroup,
     Point
 } from "react-simple-maps";
 import axios from "axios";
-import { Spinner } from "react-bootstrap";
-import moment from "moment";
+import {Spinner} from "react-bootstrap";
+import {config} from "../config";
+import {launchParams} from '../utils/typesConfig'
 
 interface Markers {
     markerOffset: number;
@@ -16,36 +18,28 @@ interface Markers {
     coordinates: Point;
 }
 
-const geoUrl = "https://raw.githubusercontent.com/zcreativelabs/react-simple-maps/master/topojson-maps/world-110m.json";
-
 const Map = () => {
 
     let markers: Markers[] = [];
     const [points, setPoints] = useState(markers);
 
-    //get current date
-    let current: string = moment().format("DD-MM-YYYY");
-    let future:string = moment().add(3, "months").format("DD-MM-YYYY");
-
     useEffect(() => {
         const fetchData = async () => {
             try {
-                const response = await axios.get(`https://ll.thespacedevs.com/2.2.0/?offset=10`);
+                const response = await axios.get(`${config.launchApi}/upcoming/`);
 
-                console.log(response);
-
-                // console.log(response.data.results);
-                // markers = response.data.results.map((l: any) => {
-                //     return {
-                //         markerOffset: 15,
-                //         name: l.name,
-                //         coordinates: [
-                //             l.location.pads[0].longitude,
-                //             l.location.pads[0].latitude,
-                //         ],
-                //     };
-                // });
-                // setPoints(markers);
+                console.log(response.data.results);
+                markers = response.data.results.map((l: launchParams) => {
+                    return {
+                        markerOffset: 15,
+                        name: l.name,
+                        coordinates: [
+                            l.pad.longitude,
+                            l.pad.latitude,
+                        ],
+                    };
+                });
+                setPoints(markers);
             } catch (error) {
                 console.log(`Error getting events data: ${error}`);
             }
@@ -55,34 +49,35 @@ const Map = () => {
 
     // @ts-ignore
     return (
-        <ComposableMap projection="geoAzimuthalEqualArea" projectionConfig={{ rotate: [58, 20, 0], scale: 400 }} >
+        <ComposableMap projection="geoAzimuthalEqualArea" projectionConfig={{rotate: [58, 20, 0], scale: 400}}>
+            <ZoomableGroup zoom={1}>
+                <Geographies geography={config.geoUrl}>
+                    {({geographies}) =>
+                        geographies.map((geo) => (
+                            <Geography key={geo.rsmKey} geography={geo}/>
+                        ))
+                    }
+                </Geographies>
 
-            <Geographies geography={geoUrl}>
-                {({ geographies }) =>
-                    geographies.map((geo) => (
-                        <Geography key={geo.rsmKey} geography={geo} />
+                {points ? (
+                    points.map(({name, coordinates, markerOffset}) => (
+                        <Marker key={name} coordinates={coordinates}>
+                            <circle r={10} fill="#F00" stroke="#fff" strokeWidth={2}/>
+                            <text
+                                textAnchor="middle"
+                                y={markerOffset}
+                                style={{fontFamily: "system-ui", fill: "#5D5A6D"}}
+                            >
+                                {name}
+                            </text>
+                        </Marker>
                     ))
-                }
-            </Geographies>
-
-            {points ? (
-                points.map(({ name, coordinates, markerOffset }) => (
-                    <Marker key={name} coordinates={coordinates}>
-                        <circle r={10} fill="#F00" stroke="#fff" strokeWidth={2} />
-                        <text
-                            textAnchor="middle"
-                            y={markerOffset}
-                            style={{ fontFamily: "system-ui", fill: "#5D5A6D" }}
-                        >
-                            {name}
-                        </text>
-                    </Marker>
-                ))
-            ) : (
-                <Spinner animation="border" />
-            )}
-    </ComposableMap>
-);
+                ) : (
+                    <Spinner animation="border"/>
+                )}
+            </ZoomableGroup>
+        </ComposableMap>
+    );
 };
 
 export default Map;
---
+++ src/config.ts
@@ -0,0 +1,4 @@
+export const config  = {
+    "geoUrl": "https://raw.githubusercontent.com/zcreativelabs/react-simple-maps/master/topojson-maps/world-110m.json",
+    "launchApi": "https://ll.thespacedevs.com/2.2.0/launch"
+}
